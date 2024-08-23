ef552a0460f2404f57ccbb743f46487aa190c0df
Evhen <35118152+Ribeyrak@users.noreply.github.com>
2024-08-22 20:00:00 -0400

    Initial Commit

---
+++ SwiftUI-ActivityView/SwiftUI_ActivityViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ActivityViewApp.swift
+//  SwiftUI-ActivityView
+//
+//  Created by Evhen Lukhtan on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ActivityViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
